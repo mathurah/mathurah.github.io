87020b15e7d14d4ed96b598839bb5a0ad3abe085
straightupjac <jdc.jaclyn@gmail.com>
2022-01-15 13:22:12 -0800

    style

--- index.jsx
+++ index.jsx
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 
 function Guestbook() {
   return (
-    <>
+    <div styles={{ mr: 4 }}>
       <SignLegacy projectId="legacyxyz" />
+      <br />
       <SignersList projectId="legacyxyz" />
-    </>
+    </div>
   );
 }
 
